228c2e94073b5cc3bf7d67e781c7836136eda746
kalunge <muthomititus+github@gmail.com>
2022-03-11 09:27:34 +0100

    reword commit message

---
+++ array_plus.rb
@@ -0,0 +1,15 @@
+class ArrayPlus
+  def self.add_one(array)
+    raise StandardError, 'Array should not be empty' if array.empty?
+
+    if array.any?(&:negative?) || array.any? do |item|
+         item > 9
+       end
+      raise StandardError,
+            'Array should not contain negative or double digit numbers'
+    end
+
+    result = array.join.to_i + 1
+    result.to_s.scan(/\d/).map(&:to_i)
+  end
+end
