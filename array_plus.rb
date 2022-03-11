cddb220bcbeb4bcb73294e0068fbe1d67adbe825
kalunge <muthomititus+github@gmail.com>
2022-03-11 09:54:26 +0100

    Add logic to retun nil if input is invalid

--- array_plus.rb
+++ array_plus.rb
@@ -1,5 +1,8 @@
 class ArrayPlus
   def self.add_one(array)
+
+    return nil if array.class.name != 'Array'
+    
     raise StandardError, 'Array should not be empty' if array.empty?
 
     if array.any?(&:negative?) || array.any? do |item|
