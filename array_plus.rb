859189593c7034548cb73b24107a764af026e638
kalunge <muthomititus+github@gmail.com>
2022-03-11 16:43:39 +0100

    Add a more optimal solution

--- array_plus.rb
+++ array_plus.rb
@@ -1,8 +1,7 @@
 class ArrayPlus
   def self.add_one(array)
-
     return nil if array.class.name != 'Array'
-    
+
     raise StandardError, 'Array should not be empty' if array.empty?
 
     if array.any?(&:negative?) || array.any? do |item|
@@ -12,7 +11,28 @@ class ArrayPlus
             'Array should not contain negative or double digit numbers'
     end
 
-    result = array.join.to_i + 1
-    result.to_s.scan(/\d/).map(&:to_i)
+    # result = array.join.to_i + 1
+    # result.to_s.scan(/\d/).map(&:to_i)
+
+
+    num = 0
+
+    output = []
+
+    array.each { |item| num = (num * 10) + item }
+    new_num = num + 1
+
+    pos = array.size - 1
+
+    while new_num
+      digit = new_num % 10
+      new_num = (new_num - digit) / 10
+
+      output[pos] = digit
+      pos -= 1
+    end
+    output
   end
 end
+
+p ArrayPlus.add_one([1, 2, 3, 5, 7])
